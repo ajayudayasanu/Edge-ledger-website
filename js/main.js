016d3d93b1f806760998063edbe0da07d22abb87
Ajay U <ajayudayasanu95@gmail.com>
2019-07-14 21:22:45 +0530

    site completed

--- js/main.js
+++ js/main.js
@@ -10,3 +10,27 @@ function initMap() {
   // The marker, positioned at location
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
